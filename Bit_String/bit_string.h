f766f6038bc8efe3af2e8b669bd1b72134332849
3omar-mostafa <42312059+3omar-mostafa@users.noreply.github.com>
2020-08-25 18:30:22 +0200

    Added push_back function

--- Bit_String/bit_string.h
+++ Bit_String/bit_string.h
@@ -1,6 +1,9 @@
 #ifndef BIT_STRING_H
 #define BIT_STRING_H
 
+#include <cstring>
+#include <cstdint>
+
 class bit_string {
 
 public:
@@ -19,11 +22,65 @@ public:
     ~bit_string();  // Destructor
 
     bit_string() = default;
+
+    void push_back(bool bit);
+
+private:
+
+    static uint64_t min(uint64_t a, uint64_t b);
+
+    void reallocate(uint32_t new_capacity_in_bytes);
+
+    void set_bit_value(uint32_t position, bool bit) const;
+
 };
 
 
 bit_string::~bit_string() {
     delete[] m_data;
+}
+
+
+void bit_string::push_back(bool bit) {
+
+    if (m_capacity_in_bytes == 0) {
+        reallocate(4);
+    }
+
+    if (m_size_in_bits == m_capacity_in_bytes * BYTE) {
+        reallocate(m_capacity_in_bytes * 2);
+    }
+
+    set_bit_value(m_size_in_bits, bit);
+    m_size_in_bits++;
+}
+
+void bit_string::set_bit_value(uint32_t position, bool bit) const {
+    uint32_t array_index = position / BYTE;
+    uint8_t bit_index = BYTE - position % BYTE - 1;
+
+    if (bit) {
+        m_data[array_index] |= 1u << bit_index;
+    } else {
+        m_data[array_index] &= ~(1u << bit_index);
+    }
+}
+
+
+void bit_string::reallocate(uint32_t new_capacity_in_bytes) {
+    auto* new_data = new uint8_t[new_capacity_in_bytes];
+    if (m_data != nullptr) {
+        uint32_t capacity = min(new_capacity_in_bytes, m_capacity_in_bytes);
+        memcpy(new_data, m_data, capacity);
+        delete[] m_data;
+    }
+    m_data = new_data;
+    m_capacity_in_bytes = new_capacity_in_bytes;
+}
+
+
+uint64_t bit_string::min(uint64_t a, uint64_t b) {
+    return (a < b) ? a : b;
 }
 
 #endif //BIT_STRING_H
